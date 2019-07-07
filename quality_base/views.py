e4d74a2d4a00ca928488947cbf39d19f7b8ccc61
oljar <oljar@tlen.pl>
2019-07-07 17:47:10 +0200

    pagination_01

--- quality_base/views.py
+++ quality_base/views.py
@@ -2,15 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Amber
 from .models import KCX
+from itertools import chain
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 @login_required
 def main_report(request):
-    posts = Amber.objects.all()
-    posts_1=  KCX.objects.all()
-    return render(request, 'quality_base/common/main_report.html', {'posts': posts,'posts_1':posts_1})
+
+    posts_1 = Amber.objects.all()
+    posts_2=  KCX.objects.all()
+    object_list = list(chain(posts_1, posts_2))
+    paginator = Paginator(object_list,3)
+    page = request.GET.get('page')
+
+    try:
+        posts =paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'quality_base/common/main_report.html', {'page':page,'posts': posts })
 
 @login_required
 def detail_report (request,year,month,day):
